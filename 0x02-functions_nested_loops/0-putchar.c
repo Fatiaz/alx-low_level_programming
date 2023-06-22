493da1b0ec2f9f26ee6fa8efc954e9496ca60b9d
fatiaz <fatiaz9944@gmail.com>
2023-06-21 17:09:59 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: "prints _putchar to studout"
+ * Return: always 0
+ */
+int main(void)
+{
+	int _putchar(char c)
+}		
+{
+	return (write(1 ,&c, 1));
+ 
